a1c5ed4267b4ab520fd08ebac0c77040c88f340a
Tsutomu Kimura <tsutomu.kimura@nifty.ne.jp>
2022-06-28 01:49:40 +0000

    change direction

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,10 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-        <Stack spacing={2} direction="row">
+        <Stack spacing={2} direction="column">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
           <Button variant="text">Text</Button>
           <Button variant="contained">Contained</Button>
           <Button variant="outlined">Outlined</Button>
