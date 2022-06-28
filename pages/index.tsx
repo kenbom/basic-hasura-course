3f70b32cacfd339a53ca91abf106b9d948bda79f
Tsutomu Kimura <tsutomu.kimura@nifty.ne.jp>
2022-06-28 01:34:34 +0000

    test cmmit

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,6 +20,9 @@ export default function Home() {
           <Button variant="text">Text</Button>
           <Button variant="contained">Contained</Button>
           <Button variant="outlined">Outlined</Button>
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
         </Stack>
       </main>
 
